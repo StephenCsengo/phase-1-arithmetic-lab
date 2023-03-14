9568b8db20c1034d8df8c361ced31d4ab874d376
Stephen Csengo <stephencsengo@gmail.com>
2023-03-14 14:29:00 -0400

    Lab completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+let random = Math.ceil((Math.random()*10));
+const num3 = 24;
+const num4 = 10;
+const mod = num3%num4;
+const max = Math.max(1, 3, 5, 20);
